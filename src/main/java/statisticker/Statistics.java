aa6226450d47b41fca93a5f30f95ba657b86c3e6
sanjeev-nnn <69623115+sanjeev-nnn@users.noreply.github.com>
2020-09-08 19:30:19 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,30 +1,36 @@
 package statisticker;
 
-import java.util.*;
+import java.util.List;
 
 public class Statistics 
 {
-        public static class Stats{
-        Float min=Float.NaN;
-        Float max=Float.NaN;
-        Float average=Float.NaN;
-                
-    }
-    
+    public static class Stats {
+		Float average;
+		Float min;
+	    Float max;
+		public Stats(float average, float min, float max) {
+			this.average=average;
+			this.min=min;
+			this.max=max;
+		}
+	}
     public static Stats getStatistics(List<Float> numbers) {
-        
-        Stats s = new Stats();
-        if(!numbers.isEmpty())
-        {
-        Float sum = 0.0f;
-        s.min = Collections.min(numbers);
-    	s.max = Collections.max(numbers);
-         for (Float n:numbers)
-        {
-            sum += n;
-        }
-        s.average = sum/numbers.size();
-        return s;
+    	if(!numbers.isEmpty()){
+    		float average=0;
+        	float min=numbers.get(0);
+        	float max=numbers.get(0);
+        	for(Float number:numbers ){
+        		if(min>number)
+        			min=number;
+        		if(max<number)
+        			max=number;
+        		average+=number;
+        	}
+        	average=average/(float)numbers.size();
+        	System.out.println(min+ " "+max+" "+average);
+        	Stats obj = new Stats(average,min,max);
+        	return obj;
+    	}
+        return (new Stats(Float.NaN,Float.NaN,Float.NaN));
     }
-            return s;
 }
