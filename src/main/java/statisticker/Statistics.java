23069485fa926034ccac52cced67372283d084db
sanjeev-nnn <69623115+sanjeev-nnn@users.noreply.github.com>
2020-09-08 19:37:47 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,13 +1,13 @@
 package statisticker;
 
-import java.util.List;
+import java.util.*;
 
 public class Statistics 
 {
     public static class Stats {
 		Float average;
 		Float min;
-	    Float max;
+	        Float max;
 		public Stats(float average, float min, float max) {
 			this.average=average;
 			this.min=min;
@@ -17,19 +17,15 @@ public class Statistics
     public static Stats getStatistics(List<Float> numbers) {
     	if(!numbers.isEmpty()){
     		float average=0;
-        	float min=numbers.get(0);
-        	float max=numbers.get(0);
-        	for(Float number:numbers ){
-        		if(min>number)
-        			min=number;
-        		if(max<number)
-        			max=number;
-        		average+=number;
-        	}
-        	average=average/(float)numbers.size();
-        	System.out.println(min+ " "+max+" "+average);
-        	Stats obj = new Stats(average,min,max);
-        	return obj;
+        	float min=Collections.min(numbers);
+        	float max=Collections.max(numbers);
+        	for (Float n:numbers)
+                {
+                sum+=n;
+                }
+        	average = sum/(numbers.size());
+        	Stats s = new Stats(average,min,max);
+        	return s;
     	}
         return (new Stats(Float.NaN,Float.NaN,Float.NaN));
     }
